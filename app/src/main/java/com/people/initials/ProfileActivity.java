cdb69d6c4ca277c93e318d4d5269c261782fa550
ruhibhandariqspl <ruhi.bhandari@quagnitia.com>
2018-05-04 11:38:13 +0530

    login

---
+++ app/src/main/java/com/people/initials/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.people.initials;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.people.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
