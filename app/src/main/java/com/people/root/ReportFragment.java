a0d2026e51e0dca9b983058d4089aeb515958f0a
ruhibhandariqspl <ruhi.bhandari@quagnitia.com>
2018-05-15 18:37:41 +0530

    Voter Search Screen

---
+++ app/src/main/java/com/people/root/ReportFragment.java
@@ -0,0 +1,92 @@
+package com.people.root;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.people.R;
+
+/**
+ * Created by admin on 5/15/2018.
+ */
+
+public class ReportFragment extends Fragment implements View.OnClickListener {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+    private Context mContext;
+    private View view;
+    private TextView tv_title;
+    private Button tv_prorities, tv_statistics,btn_back;
+
+    public ReportFragment() {
+        // Required empty public constructor
+    }
+
+    public static ReportFragment newInstance(String param1, String param2) {
+        ReportFragment fragment = new ReportFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_report, container, false);
+        mContext = getActivity();
+        initUI();
+        initListener();
+        return view;
+    }
+
+
+    public void initUI() {
+        ((DashboardActivity) mContext).floatingActionButton.setVisibility(View.GONE);
+        Typeface font = Typeface.createFromAsset(mContext.getAssets(), "Caviar_Dreams_Bold.ttf");
+        tv_title = (TextView) view.findViewById(R.id.tv_title);
+        tv_title.setTypeface(font);
+        tv_prorities = (Button) view.findViewById(R.id.tv_prorities);
+        tv_statistics = (Button) view.findViewById(R.id.tv_statistics);
+        btn_back=(Button)view.findViewById(R.id.btn_back);
+    }
+
+    public void initListener() {
+        tv_prorities.setOnClickListener(this);
+        tv_statistics.setOnClickListener(this);
+        btn_back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_prorities:
+                break;
+            case R.id.tv_statistics:
+                break;
+            case R.id.btn_back:
+                ((DashboardActivity)mContext).callSetupFragment(DashboardActivity.SCREENS.REQUEST,"Request");
+                break;
+        }
+    }
+}
