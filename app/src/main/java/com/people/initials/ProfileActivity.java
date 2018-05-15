a6261a8bb1298dcea58fd392afb558887f7730cf
shadaf <shadaf.shaikh@quagnitia.com>
2018-05-15 11:08:10 +0530

    Dashboard

--- app/src/main/java/com/people/initials/ProfileActivity.java
+++ app/src/main/java/com/people/initials/ProfileActivity.java
@@ -1,9 +1,12 @@
 package com.people.initials;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.people.R;
+import com.people.root.DashboardActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -11,5 +14,11 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        findViewById(R.id.btn_reg).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, DashboardActivity.class));
+            }
+        });
     }
 }
