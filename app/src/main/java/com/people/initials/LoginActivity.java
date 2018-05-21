3433eb338decbfec0c7e9ad0e12dffb9cccaafc9
ruhibhandariqspl <ruhi.bhandari@quagnitia.com>
2018-05-21 14:49:59 +0530

    login details

--- app/src/main/java/com/people/initials/LoginActivity.java
+++ app/src/main/java/com/people/initials/LoginActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.people.R;
@@ -12,6 +15,9 @@ import com.people.R;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView tv_title;
+    private Button btn_send_otp,btn_login;
+    private CheckBox chk_vise;
+    private LinearLayout ll_otp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +31,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         tv_title=(TextView)findViewById(R.id.tv_title);
         Typeface font = Typeface.createFromAsset(getAssets(), "Caviar_Dreams_Bold.ttf");
         tv_title.setTypeface(font);
+        btn_send_otp=(Button)findViewById(R.id.btn_send_otp);
+        chk_vise=(CheckBox)findViewById(R.id.chk_vise);
+        btn_login=(Button)findViewById(R.id.btn_login);
+        ll_otp=(LinearLayout)findViewById(R.id.ll_otp);
+        btn_send_otp.setVisibility(View.VISIBLE);
+        btn_login.setVisibility(View.GONE);
+        chk_vise.setVisibility(View.GONE);
+        ll_otp.setVisibility(View.GONE);
     }
 
     public void initListener()
     {
-        findViewById(R.id.btn_login).setOnClickListener(this);
+        btn_login.setOnClickListener(this);
+        btn_send_otp.setOnClickListener(this);
     }
 
     @Override
@@ -38,6 +53,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
             case R.id.btn_login:
                 startActivity(new Intent(this, ProfileActivity.class));
+                btn_send_otp.setVisibility(View.GONE);
+                break;
+
+            case R.id.btn_send_otp:
+                btn_send_otp.setVisibility(View.GONE);
+                btn_login.setVisibility(View.VISIBLE);
+                chk_vise.setVisibility(View.VISIBLE);
+                ll_otp.setVisibility(View.VISIBLE);
                 break;
 
         }
